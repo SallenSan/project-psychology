c9144ee1e95afcbec0a43e3a7250d411a5ca2191
Sallenkon S. Almeida <sallenkon54@gmail.com>
2024-02-17 14:29:23 -0300

    comentarios

--- psychologistApp/src/main/java/com/br/psychology/system/psychologist_system/repositories/PatientRepository.java
+++ psychologistApp/src/main/java/com/br/psychology/system/psychologist_system/repositories/PatientRepository.java
@@ -1,15 +1,18 @@
-package com.br.psychology.system.psychologist_system.repositories;
+package com.br.psychology.system.psychologist_system.repositories; // Pacote onde está localizada a interface de repositório
 
-import com.br.psychology.system.psychologist_system.models.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import com.br.psychology.system.psychologist_system.models.Patient; // Importação da entidade Patient
+import org.springframework.data.jpa.repository.JpaRepository; // Importação do JpaRepository
+import org.springframework.data.jpa.repository.Query; // Importação da anotação Query
+import org.springframework.data.repository.query.Param; // Importação da anotação Param
+import org.springframework.stereotype.Repository; // Importação da anotação Repository
 
-import java.util.List;
+import java.util.List; // Importação da classe List
 
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-    @Query("SELECT p FROM Paciente p WHERE UPPER(p.name) LIKE CONCAT('%', UPPER(:name), '%')")
-    List<Patient> findByName(@Param("name") String name);
+@Repository // Marca esta interface como um repositório Spring
+public interface PatientRepository extends JpaRepository<Patient, Long> { // Interface de repositório que estende JpaRepository
+
+    // Consulta personalizada para buscar pacientes por nome
+    @Query("SELECT p FROM Patient p WHERE UPPER(p.name) LIKE CONCAT('%', UPPER(:name), '%')")
+    List<Patient> findByName(@Param("name") String name); // Método que retorna uma lista de pacientes com base no nome
+
 }
