c17f22f3e8c32d123bef667b2feba487f88e2529
Sallenkon S. Almeida <sallenkon54@gmail.com>
2024-04-14 13:28:31 -0300

    finalizado appointment controller

--- psychologistApp/src/main/java/com/br/psychology/system/psychologist_system/controllers/AppointmentController.java
+++ psychologistApp/src/main/java/com/br/psychology/system/psychologist_system/controllers/AppointmentController.java
@@ -7,6 +7,10 @@ import com.br.psychology.system.psychologist_system.repositories.AppointmentRepo
 import com.br.psychology.system.psychologist_system.repositories.PatientRepository;
 import com.br.psychology.system.psychologist_system.repositories.PsychologistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
@@ -73,7 +78,47 @@ public class AppointmentController {
         String userMail = authentication.getName();
 
         Psychologist psychologist = psychologistRepository.findByEmail(userMail);
+        if(psychologist == null){
+            return "redirect:/psychologists/";
+        }
 
+        Appointment newAppointment = new Appointment();
+        newAppointment.setPatient(patient);
+        newAppointment.setReceita(appointment.getReceita());
+        newAppointment.setPsychologist(psychologist);
+
+        appointmentRepository.save(newAppointment);
+
+        // Redireciona para o método que realiza o download do PDF
+        return "redirect:/psychologists/appointment" + newAppointment.getId() + "/download";
+    }
+
+        // metodo que irá gerar o pdf da consulta e fará o download
+    @GetMapping("/appointment/{id}/download")
+    public ResponseEntity<byte[]> downloadPDFAppointment(@PathVariable Long id){
+        Optional<Appointment> appointmentOptional = appointmentRepository.findById(id);
+        if(!appointmentOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Appointment appointment = appointmentOptional.get();
+        Psychologist psychologist = appointment.getPsychologist();
+
+        if(psychologist == null){
+            return ResponseEntity.badRequest().body("O psicólogo associado a consulta é nulo!".getBytes());
+        }
+
+        try{
+            byte[] pdfBytes = pdfController.generatePDFAppointment(appointment);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_PDF);
+            httpHeaders.setContentDispositionFormData("attachment", "Receita" + appointment.getPatient().getName()+ "pdf");
+
+            return ResponseEntity.ok().header(httpHeaders).body(pdfBytes);
+        } catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
 
